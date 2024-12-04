35cf150a434a44dea4dcda66cd0c798c3a141088
Alfredina-A <daldinart@gmail.com>
2024-12-04 10:27:15 +0000

    adding all changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-function askgender(){ let gender = prompt (`Please enter your gender: Male or Female`);
-    if (gender === `Female`){
+function askgender(){ let gender = prompt (`Please enter your gender: Male or Female`) 
+    gender =  gender.toLowerCase();
+    if (gender === `female`){
         alert(`"You are a ${gender}! 👧🏽"`);
     }else if (gender === `Male`){
         alert (`"You are a ${gender}! 👦🏽"`);
